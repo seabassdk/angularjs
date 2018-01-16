1d81f0e475c84c0ae99a2854f23576da731de239
seabassdk <s.blichfeldt@live.com>
2018-01-16 15:46:12 +0100

    First Version assignment 1

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -6,16 +6,37 @@ angular.module('LunchCheck', [])
 
 LCController.$inject = ['$scope'];
 function LCController($scope) {
-  $scope.name = "Yaakov";
-  $scope.stateOfBeing = "hungry";
+  $scope.foods = "";
 
-  $scope.sayMessage = function () {
-    return "Yaakov likes to eat healthy snacks at night!";
+  $scope.check = function () {
+    var foodSplit = $scope.foods.split(",");
+
+    for(var i = 0; i < foodSplit.length; ++i){
+	      if (foodSplit[i] === "") foodSplit.splice(i, 1);
+    }
+
+    //if(foodSplit.length == 1 && foodSplit[0] == ""){
+    if(foodSplit.length === 0){
+      $scope.verdict = "Please enter data first";
+      $scope.myStyle = {"color":"red",
+                        "border":"1px solid red",
+                        "display":"inline-block",
+                        "padding":"3px"}
+    }else if(foodSplit.length <= 3){
+      $scope.verdict = "Enjoy! ";
+      $scope.myStyle = {"color":"green",
+                        "border":"1px solid green",
+                        "display":"inline-block",
+                        "padding":"3px"}
+    }else{
+      $scope.verdict = "Too much!";
+      $scope.myStyle = {"color":"green",
+                        "border":"1px solid green",
+                        "display":"inline-block",
+                        "padding":"3px"}
+    }
   };
 
-  $scope.feedYaakov = function () {
-    $scope.stateOfBeing = "fed";
-  };
 }
 
 })();
